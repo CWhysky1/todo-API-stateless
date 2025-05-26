d49bc3dbf560cab67c767aad298512708fbf0026
Manuel <Manuel@Manuels-MacBook-Air-4.local>
2025-05-26 16:41:06 -0700

    making changes to mytables.js and downgrading node version and sqlite to be compatable with Glitch

--- mytables.js
+++ mytables.js
@@ -1,5 +1,5 @@
 
-const Database = require("better-sqlite3");
+const Database = require("sqlite3").verbose();
 const path = require("path");
 
 const isGlitch = process.env.PROJECT_DOMAIN !== undefined;
@@ -7,24 +7,28 @@ const dbPath = isGlitch
   ? path.join(__dirname, ".data", "mystore.sqlite")
   : path.join(__dirname, "mystore.sqlite");
 
-//const DBSOURCE = path.join(__dirname, ".data", "mystore.sqlite");
-
-const db = new Database(dbPath)
-  
-try {
-    db.prepare(`
-      CREATE TABLE IF NOT EXISTS Todo (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        priority TEXT NOT NULL,
-        isComplete INTEGER DEFAULT 0,
-        isFun INTEGER DEFAULT 0
-      )
-    `).run();
-  
-    console.log("Todo table created (if it didn't already exist).");
-  } catch (err) {
-    console.error("Error setting up the database:", err.message);
+const db = new Database(dbPath, (err) => {
+  if (err) {
+    console.error("Could not connect to the database:", err.message);
+  } else {
+    console.log("Connected to the SQLite database.");
   }
-  
-  module.exports = db;
+});
+
+try {
+  db.prepare(`
+    CREATE TABLE IF NOT EXISTS Todo (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      name TEXT NOT NULL,
+      priority TEXT NOT NULL,
+      isComplete INTEGER DEFAULT 0,
+      isFun INTEGER DEFAULT 0
+    )
+  `).run();
+
+  console.log("Todo table created (if it didn't already exist).");
+} catch (err) {
+  console.error("Error setting up the database:", err.message);
+}
+
+module.exports = db;
